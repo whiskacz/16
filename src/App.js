dabdf2e22701d31ba7324e7f9706e91279d153f8
whiskacz <pawelblasiak1985@gmail.com>
2023-03-20 20:02:22 +0100

    React CSS exercise 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import  React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    active1: false,
+    active2: false
+  }
+  render(){
+
+const btn_off = {
+  border: '2px solid black',
+  padding: '10px 20px',
+  fontFamily: "arial",
+  fontSize: 30,
+  display: 'block',
+  margin: '20px auto',
+  backgroundColor: 'white',
+
+}
+const btn_on = {
+  border: '2px solid black',
+  padding: '10px 20px',
+  fontFamily: "arial",
+  fontSize: 30,
+  display: 'block',
+  margin: '20px auto',
+  backgroundColor: 'cadetblue',
+  color: 'white',
+
+}
+
+let btn = {
+  border: '2px solid black',
+  padding: '10px 20px',
+  fontFamily: "arial",
+  fontSize: 30,
+  display: 'block',
+  margin: '20px auto',
+}
+if(this.state.active2){
+  btn.backgroundColor = 'cadetblue';
+  btn.color = 'white';
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={this.state.active1 ? btn_on : btn_off} onClick={() => {this.setState({active1: !this.state.active1})}}>{this.state.active1 ? "WŁĄCZ" : "WYŁĄCZ"}</button>
+      <button style={btn} onClick={() => {this.setState({active2: !this.state.active2})}}>{this.state.active2 ? "WŁĄCZ" : "WYŁĄCZ"}</button>
+    
     </div>
   );
 }
-
+}
 export default App;
